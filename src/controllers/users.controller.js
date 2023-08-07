b603f1bcce059ad230a71d0fc997d7339baf3ac7
andre <demaiam@gmail.com>
2023-08-06 22:14:17 -0300

    testing some fixes 2

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -47,7 +47,7 @@ export async function getUserInfo(req, res) {
 
   try {
     const user = await db.query(`
-      SELECT sessions."userId" AS id, users.name, CAST((SUM("visitCount") AS "visitCount") AS int)
+      SELECT sessions."userId" AS id, users.name, CAST(SUM("visitCount") AS INT) AS "visitCount"
         FROM sessions
         JOIN users ON sessions."userId" = users.id
         JOIN urls ON sessions."userId" = urls."userId"
